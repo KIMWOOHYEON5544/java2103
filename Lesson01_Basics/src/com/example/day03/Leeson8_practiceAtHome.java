87d007c98bf61eafee784228ffbc3c1148b03774
경민 <@203.251.3.68>
2021-03-18 08:51:13 +0900

    2021.03.18 오전 8시 50분 집에서 연습

---
+++ Lesson01_Basics/src/com/example/day03/Leeson8_practiceAtHome.java
@@ -0,0 +1,44 @@
+package com.example.day03;
+
+public class Leeson8_practiceAtHome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//부모님 나이
+//		int fatherAge = 45;
+//		int motherAge = 47;
+//		
+//		char ch;
+//		ch = (fatherAge > motherAge) ? 'T' : 'F';
+//		
+//		System.out.println(ch);
+		
+		
+		
+		//비트논리연산자
+		int num1 = 5;
+		int num2 = 10;
+		int result = num1 & num2;
+		System.out.println(result);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Lesson01_Basics/src/com/example/day04/Lesson01_practice.java
@@ -0,0 +1,65 @@
+package com.example.day04;
+
+public class Lesson01_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//나이에 따라 다른 문장 출력하기
+		
+//		int age = 7;
+//		if(age >= 8) {
+//			System.out.println("학교에 다닙니다.");
+//		}  else { 
+//			System.out.println("학교에 다니지 않습니다.");   //이걸 왜 else와 같은 줄에 적으면 출력이 안되지?
+//		}
+		
+		
+		
+		//입장요금 계산하기
+		
+		int age = 29;
+		int charge;
+		
+		if(age < 8) {
+			charge = 1000;
+			System.out.println("미취학아동");
+		}
+		else if(age < 14) {
+			charge = 2000;
+			System.out.println("초등학생");
+		}
+		else if(age < 20) {
+			charge = 3000;
+			System.out.println("중,고등학생");
+		}
+		else {
+			charge = 4000;
+			System.out.println("성인");
+		}
+		System.out.println("입장료는" + charge + "원입니다.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
