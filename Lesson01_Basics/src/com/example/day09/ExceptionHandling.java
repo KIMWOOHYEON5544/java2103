e87e3192a239a6b21b7b5c32b56d252b3f015a1b
flwldms <flwldmsok@gmail.com>
2021-03-25 16:28:55 +0900

    2021.03.25   수업시간에

---
+++ Lesson01_Basics/src/com/example/day09/ExceptionHandling.java
@@ -0,0 +1,20 @@
+package com.example.day09;
+
+public class ExceptionHandling {
+
+	public static void main (String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 10;
+		int num2 = 0;
+		double result =0;
+		
+		 result = num1/num2 ;
+		
+		
+		
+		System.out.println(result);
+		System.out.println("프로그램 종료");
+	}
+
+}
